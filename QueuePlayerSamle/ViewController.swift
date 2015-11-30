028e885e0f7c711aa5ab03a056590e22dbeafb4e
Denis Melenevsky <dmelenevsky@deniss-macbook-pro.local>
2015-11-30 16:30:33 +0200

    30.11.2015 at office

--- QueuePlayerSamle/ViewController.swift
+++ QueuePlayerSamle/ViewController.swift
@@ -3,13 +3,23 @@
 //  QueuePlayerSamle
 //
 //  Created by Denis Melenevsky on 30/11/15.
-//  Copyright © 2015 Denis Melenevsky. All rights reserved.
+//  Copyright © 2015 Denis Melenevsky. Some rights reserved.
 //
 
 import UIKit
+import AVKit
+import MediaPlayer
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MPMediaPickerControllerDelegate, AVPlayerViewControllerDelegate {
+    /*
+    @IBOutlet weak var mediaPicker: MPMediaPickerController?
+    @IBOutlet weak var playerViewController: AVPlayerViewController? {
+        return self.storyboard.segu
+    }
+     */
+    
+    @IBOutlet weak var chooseButton: UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let segueIdentifier = segue.identifier {
+            switch segueIdentifier {
+                
+            case ViewController.mediaPickerSegueIdentifier:
+                if let pickerController = segue.destinationViewController as? MPMediaPickerController {
+                    pickerController.delegate = self
+                }
+                
+            default:
+                break;
+            }
+        }
+    }
+    
+    func mediaPicker(mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        for mediaItem in mediaItemCollection.items {
+            if let itemUrl = mediaItem.assetURL {
+                self.playerItems.append(AVPlayerItem(URL: itemUrl));
+            }
+        }
+        
+        mediaPicker.dismissViewControllerAnimated(true) { () -> Void in }
+    }
+    
+    static private let mediaPickerSegueIdentifier = "mediaPickerSegueIdentifier"
+    static private let playerSegueIdentifier = "playerSegueIdentifier"
+    
+    private var playerItems = [AVPlayerItem]()
 
 }
 
